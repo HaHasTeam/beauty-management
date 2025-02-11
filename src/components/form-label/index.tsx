3538d8ab1ea36ffd5ecc3e071aceee53b9cc07ca
ngockim9047 <ngockim9047@gmail.com>
2025-02-11 21:00:24 +0700

    feat: add address and update address

--- src/components/form-label/index.tsx
+++ src/components/form-label/index.tsx
@@ -5,11 +5,12 @@ import { FormLabel } from '@/components/ui/form'
 type Props = HtmlHTMLAttributes<HTMLDivElement> & {
   required?: boolean
   children: React.ReactNode
+  htmlFor?: string
 }
 
-const index = ({ required, children, className }: Props) => {
+const index = ({ required, children, className, htmlFor }: Props) => {
   return (
-    <FormLabel className={`flex items-center gap-1 ${className}`}>
+    <FormLabel htmlFor={htmlFor} className={`flex items-center gap-1 ${className}`}>
       {required && <span className='text-destructive'>*</span>}
       {children}
     </FormLabel>
